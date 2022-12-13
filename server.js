ca18209f800b155efefc8f6645b897e51e8f6fac
105-teacher <ylapidot@gmail.com>
2022-12-13 20:15:24 +0200

    Env Addition and a few woerkarounds

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+// הפעלת פונקציה מתוך ספריית אי אן וי שטוענת את ההגדרות מקובץ דוט אי אן וי
+require('dotenv').config();
+function SendEmail(u,p)
+{
+    console.log("sent Email to " + u);
+    // קרא לגוגל והתחבר באמצעו השם והסיסמה
+}
+SendEmail(process.env.USER_EMAIL,procees.env.EMAIL_PASS);
 const http=require('http');
 const app=require('./app');
 const port=5050;
