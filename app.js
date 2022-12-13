3fc0884f62233cd2207547b58ebda4c4d04216f8
105-teacher <ylapidot@gmail.com>
2022-12-13 19:09:41 +0200

    Correcting memory bugs of server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 const Productrouter=require('./api/v1/routes/product');
-
+ //
 // הוספת שכבה של ניתוב עבור מוצרים
 app.use("/product",Productrouter);
 
